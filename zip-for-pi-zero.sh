46b28ab23038406d54bd66c6ccd27b3e85e55d9e
Matt Gross <grossm04@gmail.com>
2022-06-03 17:21:27 +0100

    Added pi zero zip script

---
+++ zip-for-pi-zero.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+now=`date +%m%m%Y_%H%M`
+newZip=`echo weatherpi-mqtt-client-$now.zip`
+exclusions="-x weatherpi-mqtt-client/ignition-gateway/\* -x weatherpi-mqtt-client/.git/\*"
+
+(cd .. && zip -r $newZip weatherpi-mqtt-client -x "weatherpi-mqtt-client/ignition-gateway**" -x "weatherpi-mqtt-client/.git**")
